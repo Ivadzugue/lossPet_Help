08a909a7742904221c89d5888c4696195c2763ea
izuniga <voniadri@gmail.com>
2022-09-15 21:50:48 -0500

    change frontend to src/main/webapp

--- src/main/webapp/js/heroDetails.js
+++ src/main/webapp/js/heroDetails.js
@@ -5,25 +5,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const heroDetailContainer = document.querySelector('#heroDetailContainer');
 
     fetch('../data/heroes.json')
-    .then(response => response.json())
-    .then(data =>{ 
+        .then(response => response.json())
+        .then(data => {
 
-        const heroFinded = data.find(hero => hero.id === heroId);
+            const heroFinded = data.find(hero => hero.id === heroId);
 
-        if(heroFinded){
-            console.log(heroDetailContainer);
+            if (heroFinded) {
+                console.log(heroDetailContainer);
 
-            heroDetailContainer.innerHTML += `
+                heroDetailContainer.innerHTML += `
             <article class="col-3">
                 <h2>${heroFinded.superhero}</h2>
-                <img src="../images/${heroFinded.id}.jpg" alt="" width="200">
+                <img src="./img/${heroFinded.id}.jpg" alt="" width="200">
                 <p><strong>Raza: </strong>${heroFinded.raza}</p>
                 <p><strong>Propietario: </strong>${heroFinded.alter_ego}</p>
             </article>
             `
 
-        }else{
-            console.log('No se encontro el heroe');
-        }
-    });
+            } else {
+                console.log('No se encontro el heroe');
+            }
+        });
 });
